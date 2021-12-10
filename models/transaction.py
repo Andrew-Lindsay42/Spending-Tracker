38f4ab912534e92e00a251e71290ba89a12b4b30
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-10 19:31:04 +0000

    Decided to add description attribute to Class incase needed later - may remove

--- models/transaction.py
+++ models/transaction.py
@@ -1,7 +1,8 @@
 class Transaction:
-    def __init__(self, amount, date, merchant = None, tag = None, id = None):
+    def __init__(self, amount, date, description = None, merchant = None, tag = None, id = None):
         self.amount = amount
         self.date = date
+        self.description = description
         self.merchant = merchant
         self.tag = tag
         self.id = id
--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -18,18 +18,25 @@ class TestTransaction(unittest.TestCase):
     def test_transaction_tag_starts_None(self):
         self.assertEqual(None, self.transaction.tag)
 
+    def test_transaction_description_starts_None(self):
+        self.assertEqual(None, self.transaction.description)
+
     def test_transaction_has_amount(self):
         self.assertEqual(1.00, self.transaction.amount)
 
     def test_transaction_has_date(self):
         self.assertEqual(datetime.date(2021, 12, 10), self.transaction.date)
 
+    def test_can_create_transaction_with_description(self):
+        transaction = Transaction(1.00, datetime.date(2021, 12, 10), 'Sandwich')
+        self.assertEqual('Sandwich', transaction.description)
+
     def test_can_create_transaction_with_merchant(self):
         merchant = Merchant('Tesco', True)
-        transaction = Transaction(1.00, datetime.date(2021, 12, 10), merchant)
+        transaction = Transaction(1.00, datetime.date(2021, 12, 10), None, merchant)
         self.assertEqual('Tesco', transaction.merchant.name)
 
     def test_can_create_transaction_with_tag(self):
         tag = Tag('Groceries', True)
-        transaction = Transaction(1.00, datetime.date(2021, 12, 10), None, tag)
+        transaction = Transaction(1.00, datetime.date(2021, 12, 10), None, None, tag)
         self.assertEqual('Groceries', transaction.tag.name)
