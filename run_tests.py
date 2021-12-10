0287dac7f3b3ad13101ab806fea70252d34306ce
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-10 16:32:11 +0000

    Set-up run_tests file

---
+++ run_tests.py
@@ -0,0 +1,10 @@
+import unittest
+ 
+from tests.test_user import TestUser
+# from tests.test_merchant import TestMerchant
+# from tests.task_tag import TestTag
+# from tests.test_transaction import TestTransaction
+ 
+ 
+if __name__ == '__main__':
+    unittest.main()
