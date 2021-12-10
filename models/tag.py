b6e15b515b93eca9b9f9139666ff136093156324
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-10 17:34:02 +0000

    Created tests for Transaction Class

--- models/tag.py
+++ models/tag.py
@@ -1,6 +1,6 @@
 class Tag:
-    def __init__(self, name, active, id = None, icon_num = 0):
+    def __init__(self, name, active, icon_num = 0, id = None):
         self.name = name
         self.active = active
-        self.id = id
         self.icon_num = icon_num
+        self.id = id
---
+++ tests/test_transaction.py
@@ -0,0 +1,35 @@
+import unittest
+import datetime
+from models.merchant import Merchant
+from models.tag import Tag
+from models.transaction import Transaction
+
+class TestTransaction(unittest.TestCase):
+
+    def setUp(self):
+        self.transaction = Transaction(1.00, datetime.date(2021, 12, 10))
+    
+    def test_transaction_id_starts_None(self):
+        self.assertEqual(None, self.transaction.id)    
+        
+    def test_transaction_merchant_starts_None(self):
+        self.assertEqual(None, self.transaction.merchant)
+    
+    def test_transaction_tag_starts_None(self):
+        self.assertEqual(None, self.transaction.name)
+
+    def test_transaction_has_amount(self):
+        self.assertEqual(1.00, self.transaction.amount)
+
+    def test_transaction_has_date(self):
+        self.assertEqual(datetime.date(2021, 12, 10), self.transaction.date)
+
+    def test_can_create_transaction_with_merchant(self):
+        merchant = Merchant('Tesco', True)
+        transaction = Transaction(1.00, datetime.date(2021, 12, 10), merchant, None)
+        self.assertEqual('Tesco', transaction.merchant.name)
+
+    def test_can_create_transaction_with_tag(self):
+        tag = Tag('Groceries', True)
+        transaction = Transaction(1.00, datetime.date(2021, 12, 10), None, tag)
+        self.assertEqual('Groceries', transaction.merchant.name)
