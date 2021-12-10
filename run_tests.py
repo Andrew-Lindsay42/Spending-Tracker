f8197d436cfc28559c239fcc3e02ae5da309ff7f
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-10 17:13:33 +0000

    Created Tag class, fixed typo in run_tests

--- run_tests.py
+++ run_tests.py
@@ -2,7 +2,7 @@ import unittest
  
 from tests.test_user import TestUser
 from tests.test_merchant import TestMerchant
-from tests.task_tag import TestTag
+from tests.test_tag import TestTag
 # from tests.test_transaction import TestTransaction
  
  
