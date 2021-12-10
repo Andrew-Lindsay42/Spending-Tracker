5b6fb59ee76d6ea57b208d5a86fd66a7e8e098a5
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-10 16:59:43 +0000

    Commented out possibly unneeded methods for User class

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
 import unittest
  
 from tests.test_user import TestUser
-# from tests.test_merchant import TestMerchant
+from tests.test_merchant import TestMerchant
 # from tests.task_tag import TestTag
 # from tests.test_transaction import TestTransaction
  
