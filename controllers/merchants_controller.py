aac96cf8142d38294b0711a69de2b53307dcb9f5
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-11 11:04:14 +0000

    Added routes for merchants

--- controllers/merchants_controller.py
+++ controllers/merchants_controller.py
@@ -5,27 +5,49 @@ merchants_blueprint = Blueprint('merchants', __name__)
 
 # INDEX
 # GET '/merchants'
+@merchants_blueprint.route('/merchants')
+def merchants():
 
+    return render_template('merchants/index.html')
 
 # SHOW
 # GET '/merchants/new'
+@merchants_blueprint.route('/merchants/new')
+def new_merchant():
 
+    return render_template('merchants/new.html')
 
 # CREATE
 # POST '/merchants'
+@merchants_blueprint.route('/merchants', methods = ['POST'])
+def create_merchant():
 
+    return redirect('merchants/new.html')
 
 # SHOW
 # GET '/merchants/<id>
+@merchants_blueprint.route('/merchants/<int:id>')
+def show_merchant(id):
 
+    return render_template('merchants/show.html')
 
 # EDIT
 # GET '/merchants/<id>/edit
+@merchants_blueprint.route('/merchants/<int:id>/edit')
+def edit_merchant(id):
 
+    return render_template('merchants/edit.html')
 
 # UPDATE
 # PUT '/merchants/<id>
+@merchants_blueprint.route('/merchants/<int:id>', methods = ['POST'])
+def update_merchant(id):
 
+    return redirect('merchants/show.html')
 
 # DELETE
 # DELETE '/merchants/<id>
+@merchants_blueprint.route('/merchants/<int:id>/delete', methods = ['POST'])
+def delete_merchant(id):
+    
+    return redirect('/merchants')
