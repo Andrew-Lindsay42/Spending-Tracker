a693346fc7b2c79d4193c93da1571f0eb126c2aa
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-12 11:21:54 +0000

    Added empty repositories

---
+++ repositories/merchant_repository.py
@@ -0,0 +1,20 @@
+from db.run_sql import run_sql
+from models.merchant import Merchant
+
+def save(merchant):
+    pass
+
+def select_all():
+    pass
+
+def select(id):
+    pass
+
+def delete_all():
+    pass
+
+def delete(id):
+    pass
+
+def update(merchant):
+    pass
---
+++ repositories/transaction_repository.py
@@ -0,0 +1,22 @@
+from db.run_sql import run_sql
+from models.merchant import Merchant
+from models.tag import Tag
+from models.transaction import Transaction
+
+def save(transaction):
+    pass
+
+def select_all():
+    pass
+
+def select(id):
+    pass
+
+def delete_all():
+    pass
+
+def delete(id):
+    pass
+
+def update(transaction):
+    pass
