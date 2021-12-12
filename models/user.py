3ec0123e129c2d472373d62957fc689207073aee
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-12 17:22:39 +0000

    Added methods for user

--- models/user.py
+++ models/user.py
@@ -5,8 +5,8 @@ class User:
         self.payday = payday
         self.id = id
 
-    # def update_budget(self, new_budget):
-    #     self.budget = new_budget
+    def update_budget(self, new_budget):
+        self.budget = new_budget
 
-    # def update_payday(self, new_payday):
-    #     self.payday = new_payday
+    def update_payday(self, new_payday):
+        self.payday = new_payday
