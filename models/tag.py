4731748708890f7ecf38ba6660a06642a1765baf
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-12 19:28:11 +0000

    Added tag methods

--- models/tag.py
+++ models/tag.py
@@ -4,3 +4,9 @@ class Tag:
         self.active = active
         self.icon_num = icon_num
         self.id = id
+
+    def update_status(self, new_status):
+        self.active = new_status
+
+    def update_icon(self, new_icon):
+        self.icon_num = new_icon
