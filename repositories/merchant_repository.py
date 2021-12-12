660c535bd6d8d459874557bbd32150d9b77ac87b
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-12 21:16:49 +0000

    Added get_active method to merchant repo

--- repositories/merchant_repository.py
+++ repositories/merchant_repository.py
@@ -42,3 +42,13 @@ def update(merchant):
     sql = "UPDATE merchants SET (name, active, icon_num) = (%s, %s, %s) WHERE id = %s"
     values = [merchant.name, merchant.active, merchant.icon_num, merchant.id]
     run_sql(sql, values)
+
+def get_active():
+    merchant_list = []
+    sql = 'SELECT * FROM merchants WHERE active = True'
+    result = run_sql(sql)
+
+    for row in result:
+        merchant = Merchant(row['name'], row['active'], row['icon_num'], row['id'])
+        merchant_list.append(merchant)
+    return merchant_list
