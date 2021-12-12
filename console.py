45f591cc07f8e0b5304b67b6e27591b51ee4bc92
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-12 11:27:29 +0000

    Added console.py for testing the repository functions

---
+++ console.py
@@ -0,0 +1,14 @@
+import datetime
+from models.merchant import Merchant
+from models.tag import Tag
+from models.transaction import Transaction
+
+import repositories.merchant_repository as merchant_repo
+import repositories.tag_repository as tag_repo
+import repositories.transaction_repository as transaction_repo
+import repositories.user_repository as user_repo
+
+# Delete all existing data in tables
+merchant_repo.delete_all()
+tag_repo.delete_all()
+transaction_repo.delete_all()
