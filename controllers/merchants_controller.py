7453609ad25a019c9389db316aa93b144982b8fb
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-13 20:42:58 +0000

    Added create route for merchant

--- controllers/merchants_controller.py
+++ controllers/merchants_controller.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+from models.merchant import Merchant
 from repositories import merchant_repository as merchant_repo
 
 merchants_blueprint = Blueprint('merchants', __name__)
@@ -16,14 +17,17 @@ def merchants():
 # GET '/merchants/new'
 @merchants_blueprint.route('/merchants/new')
 def new_merchant():
-
+    
     return render_template('merchants/new.html')
 
 # CREATE
 # POST '/merchants'
 @merchants_blueprint.route('/merchants/new', methods = ['POST'])
 def create_merchant():
-
+    name = request.form['name']
+    icon = request.form['icon']
+    new_merchant = Merchant(name, True, icon)
+    merchant_repo.save(new_merchant)
     return redirect(request.referrer)
 
 # SHOW
