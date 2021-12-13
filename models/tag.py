9df4aac1e89c5fe8b2e53cd5f8527adefc7547b3
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-13 12:03:04 +0000

    Added method for auto-capitalising of name

--- models/tag.py
+++ models/tag.py
@@ -1,6 +1,6 @@
 class Tag:
     def __init__(self, name, active, icon_num = 0, id = None):
-        self.name = name
+        self.name = name.capitalize()
         self.active = active
         self.icon_num = icon_num
         self.id = id
