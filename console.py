a2df908673058ec88e42639af4c46b2f24126023
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-06 18:24:34 +0000

    Updated to create some transactions for the past 2 weeks

--- console.py
+++ console.py
@@ -16,180 +16,60 @@ transaction_repo.delete_all()
 merchant_repo.delete_all()
 tag_repo.delete_all()
 
-# Add some data to tables
-default_user = User('user')
+# Add a user who has a budget of £500
+default_user = User('user', 500)
 user_repo.save(default_user)
 
-tesco = Merchant('Tesco', True)
-waitrose = Merchant('Waitrose', True, 2)
-amazon = Merchant('Amazon', True)
+# Add some merchants
+amazon = Merchant('Amazon', False)
 greggs = Merchant('Greggs', True)
-merchant_repo.save(tesco)
-merchant_repo.save(waitrose)
+scotrail = Merchant('ScotRail', False)
+tesco = Merchant('Tesco', True)
+vue = Merchant('Vue', True)
+waitrose = Merchant('Waitrose', True)
 merchant_repo.save(amazon)
 merchant_repo.save(greggs)
+merchant_repo.save(scotrail)
+merchant_repo.save(tesco)
+merchant_repo.save(vue)
+merchant_repo.save(waitrose)
 
-groceries = Tag('groceries', True)
-electronics = Tag('ELECTRONICS', True, 3)
-eating_out = Tag('Eating out', False)
+# Add some tags
+eating_out = Tag('Eating out', True)
+electronics = Tag('Electronics', True)
+entertainment = Tag('Entertainment', True)
+groceries = Tag('Groceries', True)
+transport = Tag('Transport', False)
+tag_repo.save(eating_out)
 tag_repo.save(electronics)
+tag_repo.save(entertainment)
 tag_repo.save(groceries)
-tag_repo.save(eating_out)
-
-weekly_shop = Transaction(28.44, datetime.date(2021, 12, 12), 'Weekly shop', tesco, groceries)
-lunch = Transaction(2.22, datetime.date(2021, 12, 8), None, greggs, eating_out)
-forgotten_spend = Transaction(7.55, datetime.date(2021, 12, 10))
-dinner = Transaction(11.95, datetime.date(2021, 11, 13), 'Dinner', None, eating_out)
-
-transaction_repo.save(weekly_shop)
-transaction_repo.save(lunch)
-transaction_repo.save(forgotten_spend)
-transaction_repo.save(dinner)
+tag_repo.save(transport)
 
 
-lunch = Transaction(2.00, datetime.date(2021, 11, 25), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 11, 26), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 11, 27), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 11, 28), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(3.00, datetime.date(2021, 11, 29), 'Three sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 11, 30), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 12, 1), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 12, 2), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(12.00, datetime.date(2021, 12, 3), 'Twelve sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 12, 4), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 12, 5), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 12, 6), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 12, 7), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 12, 8), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 12, 9), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 12, 10), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(1.70, datetime.date(2021, 12, 11), 'Steak bake', greggs, eating_out)
-transaction_repo.save(lunch)
-lunch = Transaction(2.00, datetime.date(2021, 12, 12), 'Two sausage rolls', greggs, eating_out)
-transaction_repo.save(lunch)
+# Add some transactions for the past 2 weeks
+today = datetime.date.today()
 
-# Print the data in tables (will test the select all method)
-# Uncomment line 52 - 70 to run
-# all_users = user_repo.select_all()
-# for user in all_users:
-#    print(user.__dict__)
-# print('\n')
-
-# all_merchants = merchant_repo.select_all()
-# for merchant in all_merchants:
-#    print(merchant.__dict__)
-# print('\n')
-
-# all_tags = tag_repo.select_all()
-# for tag in all_tags:
-#    print(tag.__dict__)
-# print('\n')
-
-# all_transactions = transaction_repo.select_all()
-# for transaction in all_transactions:
-#    print(transaction.__dict__)
-# print('\n')
-
-# Print a selected row (will test the select method)
-# Uncomment line 74 - 84 to run
-# print(user_repo.select(default_user.id).__dict__)
-# print('\n')
-
-# print(merchant_repo.select(tesco.id).__dict__)
-# print('\n')
-
-# print(tag_repo.select(eating_out.id).__dict__)
-# print('\n')
-
-# print(transaction_repo.select(lunch.id).__dict__)
-# print('\n')
-
-# # Delete a specific row in table
-# user_repo.delete(user_with_info.id)
-# merchant_repo.delete(greggs.id)
-# tag_repo.delete(eating_out.id)
-# transaction_repo.delete(dinner.id)
-
-# Update a specific row in table
-default_user.budget = 100
-user_repo.update(default_user)
-
-tesco.active = False
-merchant_repo.update(tesco)
-
-groceries.icon_num = 10
-tag_repo.update(groceries)
-
-forgotten_spend.description = "Can't remember what this was"
-transaction_repo.update(forgotten_spend)
-forgotten_spend.description = 'Bougie pie'
-transaction_repo.update(forgotten_spend)
-forgotten_spend.merchant = waitrose
-transaction_repo.update(forgotten_spend)
-forgotten_spend.tag = groceries
-transaction_repo.update(forgotten_spend)
-
-# Get a list of all the active merchants/tag
-# active_merchants = merchant_repo.get_active()
-# for merchant in active_merchants:
-#    print(merchant.__dict__)
-# print('\n')
-
-# active_tags = tag_repo.get_active()
-# for tag in active_tags:
-#    print(tag.__dict__)
-# print('\n')
-
-# # Get a list of all the inactive merchants/tags
-# inactive_merchants = merchant_repo.get_inactive()
-# for merchant in inactive_merchants:
-#    print(merchant.__dict__)
-# print('\n')
-
-eating_out = Tag('Eating out', False)
-tag_repo.save(eating_out)
-
-# inactive_tags = tag_repo.get_inactive()
-# for tag in inactive_tags:
-#    print(tag.__dict__)
-# print('\n')
-
-# Get transactions from past day
-today_shop = Transaction(1.00, datetime.date.today(), 'Today shop')
-yesterday = datetime.date.today() - datetime.timedelta(days = 1)
-yesterday_shop = Transaction(20.00, yesterday, 'Yesterday shop', amazon, electronics)
-day_before_yesterday = datetime.date.today() - datetime.timedelta(days = 2)
-day_before_yesterday_shop = Transaction(2.00, day_before_yesterday, 'Day before yesterday shop')
+today_shop = Transaction(9.30, today, 'Odds and ends', waitrose, groceries)
+yesterday_shop = Transaction(0.89, today - datetime.timedelta(days = 1), 'Milk', tesco, groceries)
 transaction_repo.save(today_shop)
 transaction_repo.save(yesterday_shop)
-transaction_repo.save(day_before_yesterday_shop)
 
-#Get user-specified list of transactions
+weekly_shop = Transaction(38.44, today - datetime.timedelta(days = 4), 'Weekly shop', tesco, groceries)
+dinner = Transaction(19.95, today - datetime.timedelta(days = 3), 'Takeaway', None, eating_out)
+transaction_repo.save(weekly_shop)
+transaction_repo.save(dinner)
 
-last_year_shop = Transaction(1.00, datetime.date(2020, 1, 12), 'Last year shop')
-pre_pandemic_shop = Transaction(2.00, datetime.date(2019, 3, 4), 'Pre pandemic shop')
-transaction_repo.save(last_year_shop)
-transaction_repo.save(pre_pandemic_shop)
+cinema = Transaction(4.99, today - datetime.timedelta(days = 5), 'Move night!', vue, entertainment)
+transaction_repo.save(cinema)
 
-# date1 = datetime.date(2019, 1, 1)
-# date2 = datetime.date.today()
+headphones = Transaction(44.99, today - datetime.timedelta(days = 8), 'New headphones', amazon, electronics)
+transaction_repo.save(headphones)
 
-# custom_dates = transaction_repo.get_custom_date(date1, date2)
-# for transaction in custom_dates:
-#     print(transaction.__dict__)
+train = Transaction(27.60, today - datetime.timedelta(days = 11), 'Train to Glasgow', scotrail, transport)
+transaction_repo.save(train)
+
+# Excessive greggs habit
+for i in range(1, 13):
+    lunch = Transaction(2.00, today - datetime.timedelta(days = i), 'Two sausage rolls', greggs, eating_out)
+    transaction_repo.save(lunch)
